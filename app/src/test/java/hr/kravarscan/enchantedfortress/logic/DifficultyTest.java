0edde15191497d549fb94516f251035fc2c2fbda
Sunny <yeungs2@seattleu.edu>
2018-05-12 15:32:15 -0700

    using the available functions/variables to test. Added getindex test

--- app/src/test/java/hr/kravarscan/enchantedfortress/logic/DifficultyTest.java
+++ app/src/test/java/hr/kravarscan/enchantedfortress/logic/DifficultyTest.java
@@ -1,30 +1,40 @@
 package hr.kravarscan.enchantedfortress.logic;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class DifficultyTest {
 
-    Difficulty test = new Difficulty(200,20,10);
+    Difficulty test;
+
+    //grabbing difficulty
+    Difficulty difficulty = test.Medium;
 
     @Test
     public void testPop(){
 
-        double testPop = test.getStartingPop();
-        assertEquals(200,testPop,.01);
+        double testPop = difficulty.getStartingPop();
+        assertEquals(100,testPop,.01);
     }
 
     @Test
     public void testSpawn(){
-        double testSpawn = test.getDemonSpawnFactor();
-        assertEquals(testSpawn,20,.01);
+        double testSpawn = difficulty.getDemonSpawnFactor();
+        assertEquals(testSpawn,1,.01);
     }
 
     @Test
     public void testPower(){
-        double testPower = test.getDemonPowerBase();
-        assertEquals(testPower,10,.01);
+        double testPower = difficulty.getDemonPowerBase();
+        assertEquals(testPower,1.02,.01);
+    }
+
+    @Test
+    //this test the getindex which sees where the medium difficulty
+    // in in the level array
+    public void testGetIndex() {
+        int index = difficulty.getIndex();
+        assertNotEquals(2,index);
     }
 
 }
