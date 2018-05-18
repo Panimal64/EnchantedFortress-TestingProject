186299eb45d32fdf12faba18873fe5d19c06400d
Sunny Yeung <freezingstormy@gmail.com>
2018-05-17 20:08:06 -0700

    added additional tests regarding easy difficulty and renamed variables

--- app/src/test/java/hr/kravarscan/enchantedfortress/logic/DifficultyTest.java
+++ app/src/test/java/hr/kravarscan/enchantedfortress/logic/DifficultyTest.java
@@ -8,24 +8,23 @@ public class DifficultyTest {
     Difficulty test;
 
     //grabbing difficulty
-    Difficulty difficulty = test.Medium;
-
+    Difficulty medium = test.Medium;
     @Test
     public void testPop(){
 
-        double testPop = difficulty.getStartingPop();
+        double testPop = medium.getStartingPop();
         assertEquals(100,testPop,.01);
     }
 
     @Test
     public void testSpawn(){
-        double testSpawn = difficulty.getDemonSpawnFactor();
+        double testSpawn = medium.getDemonSpawnFactor();
         assertEquals(testSpawn,1,.01);
     }
 
     @Test
     public void testPower(){
-        double testPower = difficulty.getDemonPowerBase();
+        double testPower = medium.getDemonPowerBase();
         assertEquals(testPower,1.02,.01);
     }
 
@@ -33,8 +32,23 @@ public class DifficultyTest {
     //this test the getindex which sees where the medium difficulty
     // in in the level array
     public void testGetIndex() {
-        int index = difficulty.getIndex();
+        int index = medium.getIndex();
         assertNotEquals(2,index);
     }
 
+    @Test
+    public void easyNotMedium() {
+        Difficulty[] levels = test.Levels;
+        Difficulty easy = levels[0];
+        assertNotSame(easy, medium);
+    }
+
+    @Test
+    public void easyPowerDifferent() {
+        Difficulty[] levels = test.Levels;
+        Difficulty easy = levels[0];
+        double easyPop = easy.getStartingPop();
+        double medPop = medium.getStartingPop();
+        assertNotSame(medPop,easyPop);
+    }
 }
